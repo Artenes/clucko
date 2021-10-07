5716be1ac3e7200cea07c9af1b12b0d7ef1dd99c
Artenes Nogueira <artenesama@gmail.com>
2021-10-07 08:16:26 -0400

    Improves API to change hour

--- app/src/main/java/io/github/artenes/clucko/Time.kt
+++ app/src/main/java/io/github/artenes/clucko/Time.kt
@@ -8,23 +8,34 @@ import java.time.format.DateTimeFormatter
 class Time(epochMilliseconds: Long = Instant.now().toEpochMilli()) {
 
     private val now = Instant.ofEpochMilli(epochMilliseconds)
+    private val today = ZonedDateTime.ofInstant(now, ZoneId.systemDefault())
 
     fun toEpochMilli() = now.toEpochMilli()
 
-    fun setTime(hour: Int, minute: Int, second: Int = 0): Time {
-        val today = ZonedDateTime.ofInstant(now, ZoneId.systemDefault())
-        val changedTime =
-            ZonedDateTime.of(today.year, today.monthValue, today.dayOfMonth, hour, minute, second, 0, today.zone)
+    fun setHour(hour: Int): Time {
+        val changedTime = ZonedDateTime.of(today.year, today.monthValue, today.dayOfMonth, hour, today.minute, today.second, today.nano, today.zone)
+        val timestamp = changedTime.toInstant().toEpochMilli()
+        return Time(timestamp)
+    }
+
+    fun setMinute(minute: Int): Time {
+        val changedTime = ZonedDateTime.of(today.year, today.monthValue, today.dayOfMonth, today.hour, minute, today.second, today.nano, today.zone)
+        val timestamp = changedTime.toInstant().toEpochMilli()
+        return Time(timestamp)
+    }
+
+    fun setSecond(second: Int): Time {
+        val changedTime = ZonedDateTime.of(today.year, today.monthValue, today.dayOfMonth, today.hour, today.minute, second, today.nano, today.zone)
         val timestamp = changedTime.toInstant().toEpochMilli()
         return Time(timestamp)
     }
 
     fun startOfDay(): Time {
-        return setTime(0, 0)
+        return setHour(0).setMinute(0)
     }
 
     fun endOfDay(): Time {
-        return setTime(23, 59, 59)
+        return setHour(23).setMinute(59).setSecond(59)
     }
 
     fun format(pattern: String): String =
--- app/src/test/java/io/github/artenes/clucko/BalanceTest.kt
+++ app/src/test/java/io/github/artenes/clucko/BalanceTest.kt
@@ -10,10 +10,10 @@ class BalanceTest {
     fun currentBalance_getsAmountForEvenList() {
 
         val clockIns = listOf(
-            ClockIn(Time().setTime(8, 0), true),
-            ClockIn(Time().setTime(12, 0), false),
-            ClockIn(Time().setTime(13, 0), true),
-            ClockIn(Time().setTime(16, 0), false)
+            ClockIn(Time().setHour(8).setMinute(0), true),
+            ClockIn(Time().setHour(12).setMinute(0), false),
+            ClockIn(Time().setHour(13).setMinute(0), true),
+            ClockIn(Time().setHour(16).setMinute(0), false)
         )
 
         val balance = Balance(clockIns)
@@ -28,11 +28,11 @@ class BalanceTest {
     fun currentBalance_getsAmountForOddList() {
 
         val clockIns = listOf(
-            ClockIn(Time().setTime(6, 30), true),
-            ClockIn(Time().setTime(11, 45), false),
-            ClockIn(Time().setTime(12, 55), true),
-            ClockIn(Time().setTime(18, 23), false),
-            ClockIn(Time().setTime(19, 12), false)
+            ClockIn(Time().setHour(6).setMinute(30), true),
+            ClockIn(Time().setHour(11).setMinute(45), false),
+            ClockIn(Time().setHour(12).setMinute(55), true),
+            ClockIn(Time().setHour(18).setMinute(23), false),
+            ClockIn(Time().setHour(19).setMinute(12), false),
         )
 
         val balance = Balance(clockIns)
